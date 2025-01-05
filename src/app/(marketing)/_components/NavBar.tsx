aa06e9c1ec9ccaf5e5180a5d1ddadaa9f840f586
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-01-05 09:52:08 +0530

    making landing page

---
+++ src/app/(marketing)/_components/NavBar.tsx
@@ -0,0 +1,15 @@
+import BrandLogo from "@/components/BrandLogo";
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <header>
+      <nav className="flex items-center gap-10 container font-semibold">
+        <Link href="/" className="mr-auto">
+          <BrandLogo />
+        </Link>
+      </nav>
+    </header>
+  )
+
+}
