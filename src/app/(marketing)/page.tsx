bbca19686d78f79f9e2f2eb705c8355704b99ba1
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-01-05 14:08:57 +0530

    adding subcription list

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,11 +1,22 @@
-import { Button } from "@/components/ui/button";
-import { SignUpButton } from "@clerk/nextjs";
-import { ArrowRightIcon } from "lucide-react";
-import Link from "next/link";
+import { Button } from "@/components/ui/button"
+import { SignUpButton } from "@clerk/nextjs"
+import { ArrowRightIcon, CheckIcon } from "lucide-react"
+import Link from "next/link"
 import { NeonIcon } from "./_icons/Neon"
-import { ClerkIcon } from "./_icons/Clerk";
-import BrandLogo from "@/components/BrandLogo";
-import { cn } from "@/lib/utils";
+import { ClerkIcon } from "./_icons/Clerk"
+import { subscriptionTiersInOrder } from "@/data/subscriptionTiers"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { formatCompactNumber } from "@/lib/formatters"
+import { ReactNode } from "react"
+import { cn } from "@/lib/utils"
+import BrandLogo from "@/components/BrandLogo"
 
 export default function HomePage() {
   return (
@@ -23,7 +34,7 @@ export default function HomePage() {
             Get started for free <ArrowRightIcon className="size-5" />
           </Button>
         </SignUpButton>
-      </section >
+      </section>
       <section className="bg-primary text-primary-foreground">
         <div className="container py-16 flex flex-col gap-16 px-8 md:px-16">
           <h2 className="text-3xl text-center text-balance">
@@ -63,12 +74,12 @@ export default function HomePage() {
           </div>
         </div>
       </section>
-      <section id="pricing" className="px-8 py-16 bg-accent/5">
+      <section id="pricing" className=" px-8 py-16 bg-accent/5">
         <h2 className="text-4xl text-center text-balance font-semibold mb-8">
           Pricing software which pays for itself 20x over
         </h2>
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 max-w-screen-xl mx-auto">
-          {subscriptionTiresInOrder.map(tier => (
+          {subscriptionTiersInOrder.map(tier => (
             <PricingCard key={tier.name} {...tier} />
           ))}
         </div>
@@ -82,7 +93,7 @@ export default function HomePage() {
             <FooterLinkGroup
               title="Help"
               links={[
-                { label: "P3 Discounts", href: "#" },
+                { label: "PPP Discounts", href: "#" },
                 { label: "Discount API", href: "#" },
               ]}
             />
@@ -98,9 +109,7 @@ export default function HomePage() {
           <div className="flex flex-col gap-8">
             <FooterLinkGroup
               title="Features"
-              links={[
-                { label: "P3 Discounts", href: "#" },
-              ]}
+              links={[{ label: "PPP Discounts", href: "#" }]}
             />
             <FooterLinkGroup
               title="Tools"
@@ -141,12 +150,11 @@ export default function HomePage() {
                 { label: "Paddle", href: "#" },
               ]}
             />
-
           </div>
         </div>
       </footer>
     </>
-  );
+  )
 }
 
 function PricingCard({
@@ -154,16 +162,16 @@ function PricingCard({
   priceInCents,
   maxNumberOfVisits,
   maxNumberOfProducts,
-  canRemoveBrading,
   canAccessAnalytics,
   canCustomizeBanner,
-}: (typeof subscriptionTierInOrder)[number]) {
+  canRemoveBranding,
+}: (typeof subscriptionTiersInOrder)[number]) {
   const isMostPopular = name === "Standard"
 
   return (
     <Card
       className={cn(
-        "relative shadow-none rounded-3xl overflow-hidden"
+        "relative shadow-none rounded-3xl overflow-hidden",
         isMostPopular ? "border-accent border-2" : "border-none"
       )}
     >
@@ -174,8 +182,69 @@ function PricingCard({
       )}
       <CardHeader>
         <div className="text-accent font-semibold mb-8">{name}</div>
+        <CardTitle className="text-xl font-bold">
+          ${priceInCents / 100} /mo
+        </CardTitle>
+        <CardDescription>
+          {formatCompactNumber(maxNumberOfVisits)} pricing page visits/mo
+        </CardDescription>
       </CardHeader>
+      <CardContent>
+        <SignUpButton>
+          <Button
+            className="text-lg w-full rounded-lg"
+            variant={isMostPopular ? "accent" : "default"}
+          >
+            Get Started
+          </Button>
+        </SignUpButton>
+      </CardContent>
+      <CardFooter className="flex flex-col gap-4 items-start">
+        <Feature className="font-bold">
+          {maxNumberOfProducts}{" "}
+          {maxNumberOfProducts === 1 ? "product" : "products"}
+        </Feature>
+        <Feature>PPP discounts</Feature>
+        {canAccessAnalytics && <Feature>Advanced analytics</Feature>}
+        {canRemoveBranding && <Feature>Remove Easy PPP branding</Feature>}
+        {canCustomizeBanner && <Feature>Banner customization</Feature>}
+      </CardFooter>
     </Card>
+  )
+}
 
+function Feature({
+  children,
+  className,
+}: {
+  children: ReactNode
+  className?: string
+}) {
+  return (
+    <div className={cn("flex items-center gap-2", className)}>
+      <CheckIcon className="size-4 stroke-accent bg-accent/25 rounded-full p-0.5" />
+      <span>{children}</span>
+    </div>
+  )
+}
+
+function FooterLinkGroup({
+  title,
+  links,
+}: {
+  title: string
+  links: { label: string; href: string }[]
+}) {
+  return (
+    <div className="flex flex-col gap-4">
+      <h3 className="font-semibold">{title}</h3>
+      <ul className="flex flex-col gap-2 text-sm">
+        {links.map(link => (
+          <li key={link.href}>
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
