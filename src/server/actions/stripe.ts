ed6e5f476985404f5483673558d48f291f63be44
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-01-16 14:13:24 +0530

    adding stripe price id

---
+++ src/server/actions/stripe.ts
@@ -0,0 +1,50 @@
+"use server"
+
+import { PaidTierNames, subscriptionTiers } from "@/data/subscriptionTiers";
+import { currentUser, User } from "@clerk/nextjs/server";
+import { getUserSubscription } from "../db/subscription";
+import { redirect } from "next/navigation";
+import { Stripe } from "stripe";
+import { env as serverEnv } from "@/data/env/server";
+import { env as clientEnv } from "@/data/env/client";
+
+const stripe = new Stripe(serverEnv.STRIPE_SECRET_KEY)
+
+export async function createCheckoutSession(tier: PaidTierNames) {
+  const user = await currentUser();
+  if (user == null) return { error: true }
+
+  const subscription = await getUserSubscription(user.id)
+  if (subscription == null) return { error: true }
+
+  if (subscription.stripeCustomerId == null) {
+    const url = await getCheckoutSession(tier, user)
+    if (url == null) return { error: true }
+    return (url)
+  } else {
+    // const url = await getSubscriptionUpgradeSession(tier, user)
+    // redirect(url)
+  }
+}
+
+async function getCheckoutSession(tier: PaidTierNames, user: User) {
+  const session = await stripe.checkout.sessions.create({
+    customer_email: user.primaryEmailAddress?.emailAddress,
+    subscription_data: {
+      metadata: {
+        clerkUserId: user.id,
+      },
+    },
+    line_items: [
+      {
+        price: subscriptionTiers[tier].stripePriceId,
+        quantity: 1,
+      },
+    ],
+    mode: "subscription",
+    success_url: `${clientEnv.NEXT_PUBLIC_SERVER_URL}/dashboard/subscription`,
+    cancel_url: `${clientEnv.NEXT_PUBLIC_SERVER_URL}/dashboard/subscription`,
+  })
+
+  return session.url
+}
